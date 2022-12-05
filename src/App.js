a17a6dfae556bf9d9845923f49da3a36f3b37a99
Saphal47 <saphalkumar13@gmail.com>
2022-12-05 19:47:31 +0530

    Modified TitlePage and ReactRouter Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React ,{useState} from 'react';
 import './App.css';
+import About from './components/About';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import Textbox from './components/Textbox';
+
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  Outlet
+} from "react-router-dom";
+
+
 
 function App() {
   const [darkmode,setDarkMode] = useState('light');
@@ -26,23 +37,36 @@ function App() {
        setDarkMode('dark');
        document.body.style.backgroundColor = 'rgb(9 5 72)';
        showAlert(" Dark Mode Enabled","Success");
+       document.title="TextApp-DarkMode";
     }
     else{
       setDarkMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert(" Light Mode Enabled","Success");
+      document.title="TextApp-Home";
     }
   }
 
   return (
-    <div className="App-header">
-      <Navbar title="TextEditor" abtText="Details" mode={darkmode} togglemode={togglemode}/>
+    <BrowserRouter>
+      <div className="App-header">
+      <Navbar title="TextEditor" abtText="About" mode={darkmode} togglemode={togglemode}/>
       <Alert alert={alert}/>
-
-      <Textbox showAlert={showAlert} heading="Enter Your message" mode={darkmode}/>
-      {/*passing it in textbox to display alert msg while clicking these btns */}
       {/*passing the alert state in this var */}
+      
+      {/*passing it in textbox to display alert msg while clicking these btns */}           
+       <Switch>
+        <Route exact path="/about">
+          <About/>
+        </Route>
+        <Route exact path="/">
+          <Textbox heading="Enter your message"  mode={darkmode} showAlert={showAlert} />
+        </Route>
+      </Switch>
+       {/*We use exact path for complete matching by React*/}
+      
     </div>
+  </BrowserRouter>  
   );
 }
 
@@ -50,3 +74,5 @@ export default App;
 
 //Now we are seeing the alerts as they are hanged and once cut they disappear
 //& load after refresh
+
+//React router will help in viewing both About and Textbox page smoothly
