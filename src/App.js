f4f43d915e1fb0652460849cf60fe7d85d2fbba8
Saphal47 <saphalkumar13@gmail.com>
2022-12-08 19:35:39 +0530

    Deploy to git removing Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React ,{useState} from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import Textbox from './components/Textbox';
 
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link,
-  Outlet
-} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   Switch,
+//   Route
+// } from "react-router-dom";
 
 
 
@@ -48,25 +46,25 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+    // <BrowserRouter>
       <div className="App-header">
       <Navbar title="TextEditor" abtText="About" mode={darkmode} togglemode={togglemode}/>
       <Alert alert={alert}/>
       {/*passing the alert state in this var */}
       
       {/*passing it in textbox to display alert msg while clicking these btns */}           
-       <Switch>
-        <Route exact path="/about">
+       {/* <Switch> */}
+        {/* <Route exact path="/about">
           <About/>
-        </Route>
-        <Route exact path="/">
+        </Route> */}
+        {/* <Route exact path="/"> */}
           <Textbox heading="Enter your message"  mode={darkmode} showAlert={showAlert} />
-        </Route>
-      </Switch>
+        {/* </Route>
+      </Switch> */}
        {/*We use exact path for complete matching by React*/}
       
     </div>
-  </BrowserRouter>  
+  // </BrowserRouter>  
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,25 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
-
+// import {a} from 'react-router-dom'
+//React Router doesn't work with About pages
 export default function Navbar(props) {
 
   return (
     <div>
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-            <Link className="navbar-brand" to="/">{props.title}</Link>
+            <a className="navbar-brand" href="#">{props.title}</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
-                <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/about">{props.abtText}</Link>
-                </li>
+                {/* <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/about">{props.abtText}</a>
+                </li> */}
             </ul>
             <div className={`form-check form-switch text-${props.mode ==='light' ? 'dark': 'light'}`}>
               <input className="form-check-input" onClick={props.togglemode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
